1b51785fe935e571356bfa467bf269369e8cb197
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-01 00:56:03 +0300

    IComparableImplament 9\1\2023

---
+++ IComparableImplament/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace IComparableImplament
+{
+    public interface ISehir
+    {
+        void Tanıt();
+        void NufusBilgisiGetir(int plakano);
+    }
+    public class sehir : ISehir,IComparable<sehir>
+    {
+        public sehir(int plakaNo ,string sehirAdi)
+        {
+            PlakaNo = plakaNo;
+            SehirAdi = sehirAdi;
+
+        }
+        public int PlakaNo { get; set; }
+
+        public string SehirAdi { get; set; }
+
+        public int CompareTo(sehir? other)
+        {
+            if (this.PlakaNo < other.PlakaNo)
+            {
+                return -1;
+            }else if(this.PlakaNo == other.PlakaNo)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public void NufusBilgisiGetir(int plakano)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Tanıt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string? ToString()
+        {
+            return $"{PlakaNo}"+$"{SehirAdi}";
+        }
+    }
+
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            //List
+            var sayilar = new List<int>()
+            {
+                53,40,14,2,3,12,15
+            };
+            sayilar.Sort();
+            sayilar.ForEach(s => Console.WriteLine(s));
+
+            //Sehir Listesi
+
+            var sehirler = new List<sehir>()
+            {
+                new sehir(6,"Ankara"),
+                new sehir(34,"İstanbul"),
+                new sehir(55,"Samsun"),
+                new sehir(23,"Elazığ")
+            };
+            sehirler.Add(new sehir(1, "Adana"));
+            sehirler.Sort();
+
+            sehirler.ForEach(s => Console.WriteLine(s));
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortedListUygulama/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedListUygulama
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            //Sorted List
+
+            var kitapIcerigi = new SortedList();
+
+            kitapIcerigi.Add(1, " Önsöz");
+            kitapIcerigi.Add(50," Değişkenler");
+            kitapIcerigi.Add(40, " Operatörler");
+            kitapIcerigi.Add(60, " Döngüler");
+            kitapIcerigi.Add(45, " İlişkisel Operatörler");
+
+            foreach(DictionaryEntry entry in kitapIcerigi)
+            {
+                Console.WriteLine($"{entry.Key}"+$"{entry.Value}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
