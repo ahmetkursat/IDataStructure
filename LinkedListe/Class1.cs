8c3632363be8fd86ca08721e7e4684043cbd48a7
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-04 21:30:47 +0300

    Linked List

---
+++ LinkedListe/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListe
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            var sehirler = new LinkedList<string>();
+
+            sehirler.AddFirst("Ordu");
+            sehirler.AddFirst("Trabzon");
+            sehirler.AddLast("İstanbul");
+            sehirler.AddAfter(sehirler.Find("Ordu"), "Samsun");
+            sehirler.AddBefore(sehirler.First.Next.Next, "Giresun");
+            sehirler.AddAfter(sehirler.Last.Previous, "Sinop");
+            sehirler.AddAfter(sehirler.Find("Sinop"), "Zonguldak");
+
+            var eleman = sehirler.First;
+            while (eleman != null)
+            {
+                Console.WriteLine(eleman.Value);
+                eleman = eleman.Next;
+                
+            }
+            var gecici = sehirler.Last;
+            while (gecici != null)
+            {
+                Console.WriteLine(gecici.Value);
+                gecici = gecici.Previous;
+
+            }
+
+            //foreach (var item in sehirler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
