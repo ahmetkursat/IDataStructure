a4d36356f90ef9800c1a5151a8f7cb3b826189ae
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-09-06 14:35:31 +0300

    Sözlük Yapısı

---
+++ _Dictionary/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Dictionary
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary
+            var telefonKodlari = new Dictionary<int, string>()
+            {
+                {332,"Konya" },
+                {424,"Elazığ" },
+                {466,"Artvin" }
+            };
+
+            //Ekleme
+            telefonKodlari.Add(322, "Adana");
+            telefonKodlari.Add(212,"İstanbul");
+            telefonKodlari.Add(216, "İstanbul");
+
+            //Erişim
+            telefonKodlari[466] = "Artvin";
+
+            //ContainKey
+            if (!telefonKodlari.ContainsKey(312))
+            {
+                Console.WriteLine("Ankaranın Kod Bilgisi Tanımlı Değil");
+                telefonKodlari.Add(312, "Ankara");
+                Console.WriteLine("Yeni Kod Eklendi");
+            };
+
+            //ContainsValue
+
+            if (!telefonKodlari.ContainsValue("Malatya"))
+            {
+                Console.WriteLine("\a Malatyanın kod bilgisi tanımlı değil");
+                telefonKodlari.Add(512, "Malatya");
+                Console.WriteLine("Yeni Kod Eklendi");
+            };
+
+            telefonKodlari.Remove(322);
+
+            foreach (var item in telefonKodlari)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
---
+++ _Queue/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Queue
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            kuyruktemeller();
+
+            var sesliHarfler = new List<char>()
+            {
+                'a','e','ı','i','u','ü','o','ö'
+            };
+
+
+            ConsoleKeyInfo secim;
+            var kuyruk = new Queue<char>();
+
+
+
+            foreach (char k in sesliHarfler)
+            {
+                Console.WriteLine();
+                Console.Write($"{k,-5} kuyruga eklensinmi? [e/h]" );
+                secim = Console.ReadKey();
+                Console.WriteLine();
+                if(secim.Key == ConsoleKey.E)
+                {
+                    kuyruk.Enqueue(k);
+                    Console.WriteLine($"\n{k,-5} kuyruga eklendi");
+                }
+
+
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+
+        private static void kuyruktemeller()
+        {
+            //tanımlama
+            var karakerKuyrugu = new Queue<char>();
+            //Ekleme
+            karakerKuyrugu.Enqueue('a');
+            karakerKuyrugu.Enqueue('b');
+
+            Console.WriteLine($"Elemnan sayisi : {karakerKuyrugu.Count}");
+
+            //Diziye atama
+
+            var dizi = karakerKuyrugu.ToArray();
+
+            //Çıkarma 
+            Console.WriteLine($"Kuyrugun başındaki eleman : {karakerKuyrugu.Peek}");
+            Console.WriteLine($"{karakerKuyrugu.Dequeue()} : kuyruktan cıkarıldı");
+            Console.WriteLine($"Eleman Sayısı {karakerKuyrugu.Count}");
+            Console.WriteLine($"Kuyrugun basındakı eleman : {karakerKuyrugu.Peek()}");
+        }
+
+
+
+    }
+}
